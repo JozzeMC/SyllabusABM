b9eb55a71aa3e1742e85701f060d86278c139bf1
JozzeMC <jozze.jr@gmail.com>
2026-04-16 18:22:21 -0600

    Update Sub of 2 numbers.java

--- Java/Sub of 2 numbers.java
+++ Java/Sub of 2 numbers.java
@@ -1,1 +1,11 @@
-
+class Main {
+    
+    public static void main(String[] args) {
+        
+        int a = 8;
+        int b = 2;
+        int sub = a - b;
+        
+        System.out.println("Sub of 2 numbers:" + sub);
+    }
+}
