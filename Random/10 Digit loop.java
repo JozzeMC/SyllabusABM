7439288390f1abab7a0e75188fe9237ee6a0cf0a
JozzeMC <jozze.jr@gmail.com>
2026-04-16 21:28:24 -0600

    Update 10 Digit loop.java

--- Random/10 Digit loop.java
+++ Random/10 Digit loop.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+      long startnumber = 123456789L;
+
+        System.out.println("\n--- Running Loop from Start to End + 100 ---");
+
+
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(startnumber + i);
+        
+        }
+    }
+}
